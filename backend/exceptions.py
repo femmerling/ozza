75088e702996a990b66db35aa51d7d932b45658e
Fauzan Emmerling <erich@emfeld.com>
2020-10-20 14:12:45 +0700

    Fixed some errors and formatting

--- backend/exceptions.py
+++ backend/exceptions.py
@@ -1,4 +1,3 @@
-
 class OzzaException(Exception):
     error_code = 500
     payload = None
@@ -11,31 +10,44 @@ class OzzaException(Exception):
         if payload:
             self.payload = payload
 
+
 class EmptyParameterException(OzzaException):
-    def __init__(self, message="Method parameter should not be empty", status_code=400)
+    def __init__(self, message="Method parameter should not be empty"):
+        OzzaException.__init__(message=message, status_code=400)
+
 
 class DirectoryCreationException(OzzaException):
     def __init__(self, message="Can't create directory"):
         OzzaException.__init__(message=message)
 
+
 class IdNotFoundException(OzzaException):
     def __init__(self, message="Value must contain `id` field"):
         OzzaException.__init__(message=message, status_code=400)
+
+
+class MismatchIdException(OzzaException):
+    def __init__(self, message="Value `id` mismatched existing `id`"):
+        OzzaException.__init__(message=message, status_code=400)
+
 
 class ResourceGroupNotFoundException(OzzaException):
     def __init__(self, message="Resource with that key was not found"):
         OzzaException.__init__(message=message, status_code=404)
 
+
 class FieldNotFoundException(OzzaException):
     def __init__(self, message="Requested field was not found in data"):
         OzzaException.__init__(message=message, status_code=400)
+
 
 class DataWriteException(IOError, OzzaException):
     def __init__(self, message="Data write error"):
         IOError.__init__(self)
         OzzaException.__init__(message=message, status_code=500)
 
+
 class DirectoryOperationException(OSError, OzzaException):
-    def __init__(self, messaage="Directory operation error"):
+    def __init__(self, message="Directory operation error"):
         OSError.__init__(self)
         OzzaException.__init__(message=message, status_code=500)
