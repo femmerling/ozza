6c37fd64864013dfd7404496d6f18c33de699479
Fauzan Erich Emmerling <erich@emfeld.com>
2023-03-01 23:26:37 +0400

    Migrate to FastAPI for resource API

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,40 +1,24 @@
-import time
-from datetime import datetime
-
+import logging
 from fastapi import FastAPI
-#from sanic import Sanic
+from app.api import resource
 
-from rest_api.api_service import ApiService
-from rest_api.member_api import MemberApi
-from rest_api.resource_api import ResourceApi
+logger = logging.getLogger(__name__)
 
-
-def create_api():
-    service = ApiService()
-    api = FastAPI()
-
-
-    @api.middleware('request')
-    async def embed_start_time(request):
-        request.ctx.start_time = time.time()
-
-    @api.middleware('response')
-    async def log_request(request, response):
-        spend_time = round((time.time() - request.ctx.start_time) * 1000)
-        print("[{}] [ACCESS] LEN:{}b\tLAT:{}ms IP:{} STATUS:{} {}\t{} {}".format(
-            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            len(response.body),
-            spend_time,
-            request.headers.get('remote_addr'),
-            response.status,
-            request.method,
-            request.path,
-            request.query_string
-        ))
-
-    api.add_route(ResourceApi.as_view(service=service), "/<resource>")
-    api.add_route(MemberApi.as_view(service=service), "/<resource>/<id_value>")
-
-    return api
+app_parameters = {
+    'title': "Ozza Web API",
+    'description': "Web interface to Ozza datastore",
+    'version': '0.1.0',
+    'docs_url': '/swagger',
+    'redoc_url': '/docs'
+}
 
 
+app = FastAPI(**app_parameters)
+
+
+@app.get('/healthcheck')
+def health_check():
+    return {"status": "OK"}
+
+
+app.include_router(resource.router, tags=['resource'], prefix='/resource')
--- src/app/utils.py
+++
@@ -1,9 +0,0 @@
-def setup_basic_params(name: str = "Ozza App") -> dict:
-    return {
-        "title": f"{name} public API",
-        "description": "{svc_name} public api",
-        "version": "0.1.0",
-        "docs_url": "/swagger",
-        "redoc_url": "/docs",
-        "openapi_prefix": "/ozza/"
-    }
